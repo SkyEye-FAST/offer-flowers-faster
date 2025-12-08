ba327ea9ee3206a83fb32e520b924c40ba6b082d
SkyEye_FAST <skyeyefast@foxmail.com>
2025-12-08 13:11:50 +0800

    init

---
+++ src/main/java/com/skyeyefast/offerflowersfaster/OfferFlowersFaster.java
@@ -0,0 +1,25 @@
+package com.skyeyefast.offerflowersfaster;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OfferFlowersFaster implements ModInitializer {
+	public static final String MOD_ID = "offer-flowers-faster";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	private static final double DEFAULT_MULTIPLIER = 4000.0D;
+
+	private static double flowerOfferMultiplier = DEFAULT_MULTIPLIER;
+
+	public static double getFlowerOfferMultiplier() {
+		return flowerOfferMultiplier;
+	}
+
+	@Override
+	public void onInitialize() {
+		flowerOfferMultiplier = Math.max(1.0D, DEFAULT_MULTIPLIER);
+		LOGGER.info("Offer Flowers Faster multiplier set to {}", flowerOfferMultiplier);
+	}
+}
---
+++ src/main/java/com/skyeyefast/offerflowersfaster/mixin/OfferFlowerGoalMixin.java
@@ -0,0 +1,16 @@
+package com.skyeyefast.offerflowersfaster.mixin;
+
+import com.skyeyefast.offerflowersfaster.OfferFlowersFaster;
+import net.minecraft.world.entity.ai.goal.OfferFlowerGoal;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(OfferFlowerGoal.class)
+public abstract class OfferFlowerGoalMixin {
+    @ModifyConstant(method = "canUse", constant = @Constant(intValue = 8000))
+    private int offerflowersfaster$adjustOfferInterval(int original) {
+        double multiplier = OfferFlowersFaster.getFlowerOfferMultiplier();
+        return (int) Math.max(1.0D, Math.round(original / multiplier));
+    }
+}
