2b85643d4e1be75bfaa1c617d43b582594da61c2
SkyEye_FAST <skyeyefast@foxmail.com>
2025-12-23 21:30:03 +0800

    feat: update build configuration for Legacy Fabric compatibility

--- src/main/java/top/skyeyefast/offerflowersfaster/OfferFlowersFaster.java
+++ src/main/java/top/skyeyefast/offerflowersfaster/OfferFlowersFaster.java
@@ -1,13 +1,10 @@
 package top.skyeyefast.offerflowersfaster;
 
 import net.fabricmc.api.ModInitializer;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class OfferFlowersFaster implements ModInitializer {
     public static final String MOD_ID = "offer-flowers-faster";
 
-    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
     public static final double DEFAULT_MULTIPLIER = 4000.0D;
 
     private static double flowerOfferMultiplier = DEFAULT_MULTIPLIER;
@@ -19,6 +16,5 @@ public class OfferFlowersFaster implements ModInitializer {
     @Override
     public void onInitialize() {
         flowerOfferMultiplier = Math.max(1.0D, DEFAULT_MULTIPLIER);
-        LOGGER.info("Offer Flowers Faster multiplier set to {}", flowerOfferMultiplier);
     }
 }
--- src/main/java/top/skyeyefast/offerflowersfaster/mixin/OfferFlowerGoalMixin.java
+++ src/main/java/top/skyeyefast/offerflowersfaster/mixin/OfferFlowerGoalMixin.java
@@ -1,14 +1,14 @@
 package top.skyeyefast.offerflowersfaster.mixin;
 
-import net.minecraft.world.entity.ai.goal.OfferFlowerGoal;
+import net.minecraft.entity.ai.goal.IronGolemLookGoal;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import top.skyeyefast.offerflowersfaster.OfferFlowersFaster;
 
-@Mixin(OfferFlowerGoal.class)
+@Mixin(IronGolemLookGoal.class)
 public abstract class OfferFlowerGoalMixin {
-    @ModifyArg(method = "canUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/RandomSource;nextInt(I)I"), index = 0)
+    @ModifyArg(method = "canStart", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I"), index = 0)
     private int offerflowersfaster$adjustOfferInterval(int original) {
         double multiplier = OfferFlowersFaster.getFlowerOfferMultiplier();
         return (int) Math.max(1.0D, Math.round(original / multiplier));
